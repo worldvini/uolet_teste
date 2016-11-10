0f458be9606668bf18d2383156b2b5dbe4f539eb
Vinicius da Silva Fleury <worldvini@hotmail.com>
2016-11-10 15:18:56 -0200

    'form_and_js_adjustments_plus_valid_arq_selenium'

--- js/form.js
+++ js/form.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-
+	//todo novo carregamento da pagina limpar os campos
 	$('#form_valid').trigger("reset");
 	$('#mensagem').val('');
-
+	//adiciona uma classe que define o background cor verde(correto) ou amarelo(incorreto) na hora do preenchimento
 	$('input').on('input', function(){
 	 	var $el = $(this);
 	 	if (this.checkValidity()) {
@@ -13,10 +13,7 @@ $(document).ready(function(){
 	    	$el.addClass('error');
 		}
 	});
-
-	//reset após correto submit
-	
-
+	//informa se as informações foram enviadas, e encaminha para a pagina home nesse caso como não tem, o proprio form.
 	$(function($) {
 		$('#form_valid').submit(function(){
 			alert('dados enviados com sucesso!');	
